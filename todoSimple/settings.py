e9b1916ba9b752ab2c2d8e80c5d2cf59f73e1aa0
jorgegso <hajorge2012@gmail.com>
2021-12-02 16:39:15 -0300

    add

--- todoSimple/settings.py
+++ todoSimple/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'todoSimple.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'nuevadatabase',
     }
 }
 
